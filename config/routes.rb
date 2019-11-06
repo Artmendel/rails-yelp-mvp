9b0b239035bba5b2bb48bbad7af738dbbbf8cd09
Christophe Duparquet <cduparquet@yahoo.com>
2019-11-06 20:51:25 +0100

    added controller for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :create, :index]
+  get 'restaurants/:id', to: 'restaurants#show'
 end
